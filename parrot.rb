53cd26fbb068ef41bb0a7edfb00c2e6eb12d7bf7
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-08 20:23:33 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(sound="Squawk")
-  puts "Parrot goes #{sound}"
-  "Parrot goes #{sound}"
+  puts #{sound}
+  #{sound}
 end 
-parrot(sound)
+parrot
