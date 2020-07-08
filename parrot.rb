a3814e5186b27b3193cc2a3bfc3f5f3477955ce7
literal-bookmark-0123 <ben.stephanus@gmail.com>
2020-07-08 20:15:13 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound="Squawk!")
-  puts "The parrot sometimes goes #{sound}"
-  "The parrot sometimes goes #{sound}"
+def parrot
+  puts "Squawk!"
+  "Squawk!"
 end 
 parrot
